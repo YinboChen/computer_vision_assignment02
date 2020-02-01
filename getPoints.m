f6c112a448de8c8f5eceb4c6687032923eb50c59
Yinbo Chen <39783798+YinboChen@users.noreply.github.com>
2020-02-01 16:52:59 -0700

    added save function and the first saved file

--- getPoints.m
+++ getPoints.m
@@ -9,32 +9,33 @@ Temp2nd = zeros(10,2);
 % the 2nd image's coordinates 10*2 matrix
 
 figure(1),imshow(inImg1),title('1st image');
-
+% Performing the first image
 for i = 1 :10
     [c1,r1] = ginput(1);
     Temp1st(i,:)= [r1,c1];
-    display = [num2str(i),'times'];
+    display = [num2str(i),' times'];
 %     display index of coordinate
     disp(display);
 end
-% use a loop to select 10 points one by one
-Temp1st
-
+% use a loop to select 10 points one by one from the 1st image
+% record into a 10*2 matrix - Temp1st
 
 imshow(inImg2),title('2nd image');
-% shows two images with both axes
+% Performing the second image
 
 for j = 1 :10
     [c2,r2] = ginput(1);
     Temp2nd(j,:)= [r2,c2];
-    display = [num2str(j),'times'];
+    display = [num2str(j),' times'];
 %     display index of coordinate
     disp(display);
 end
+% use a loop to select 10 points one by one from the 2nd image
+% record into a 10*2 matrix - Temp2nd
 
-Temp2nd
-for m =1:10
-    output(m,:)
-end
-output
+
+output = [Temp1st,Temp2nd];
+% combine twe matrix(Temp1st & Temp2nd) to a 10*4 matrix as an output
+save('output.mat','output')
+disp('Save output matrix to file output.mat!!!')
 end
